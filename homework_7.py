f3ae77c6fd265ca05dc293afb4cec16f1be92e16
BAIELLLLLLL <tybser102@gmail.com>
2026-08-27 19:37:26 +0600

    Первый коммит

---
+++ homework_7.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+
+connection = sqlite3.connect("library.db")
+
+
+def create_table():
+    connection.execute("""
+        CREATE TABLE IF NOT EXISTS books (
+            name TEXT,
+            author TEXT,
+            publication_year INTEGER,
+            genre TEXT,
+            number_of_pages INTEGER,
+            number_of_copies INTEGER
+        )
+    """)
+    connection.commit()
+
+
+def insert_books():
+    books = [
+        ("Война и мир", "Лев Толстой", 1869, "Роман", 122)
+        ]
